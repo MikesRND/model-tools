d9f1f462726df8af8f75ac65c97cafde43ed06c8
Mike Babst <6515522+MikesRND@users.noreply.github.com>
2025-03-20 09:06:38 -0400

    Update newComponent.m
    Add status

--- model_tools/ModelFrame/newComponent.m
+++ model_tools/ModelFrame/newComponent.m
@@ -23,6 +23,7 @@ if exist(compDir, 'dir')
 end
 
 % Create directory structure
+fprintf('Creating component folders...')
 mkdir(compDir);
 modelDir = fullfile(compDir,'model');
 testDir = fullfile(compDir,'test');
@@ -30,19 +31,25 @@ codegenDir = fullfile(compDir,'codegen');
 mkdir(modelDir)
 mkdir(testDir)
 mkdir(codegenDir)
+fprintf('Done\n')
 
 % Create Project
+fprintf('Creating project...')
 proj = createModelProject(componentName, 'projectFolder', compDir);
+fprintf('Done\n')
 
 % Create new componnet in its own simuilnk library
+fprintf('Creating Simulink library component...')
 cd(modelDir);
 initFunc = createComponentInitScript(componentName, opts.style); % Create component init script
 libName = createSimLibrary(componentName, initFunc, opts.style); % Model itself
 
 newfile = proj.addFolderIncludingChildFiles(modelDir);
 proj.addPath(modelDir);
+fprintf('Done\n')
 
 % Create testbench
+fprintf('Creating component testbench model...')
 cd(testDir);
 tbName = createSimTestbench(componentName, libName, opts.style);
 testScript = createComponentTestScript(componentName);
@@ -50,17 +57,21 @@ testScript = createComponentTestScript(componentName);
 newfile = proj.addFolderIncludingChildFiles(testDir);
 proj.ProjectStartupFolder=testDir;
 proj.SimulinkCodeGenFolder=codegenDir;
+fprintf('Done\n')
 
 % Cleanup
+fprintf('Cleaning up...')
 cd (libFolder);
 fclose('all');
+proj.close();
+fprintf('Done\n')
 
-fprintf('Finished creating module: %s\n', componentName);
-fprintf('To view model and run simulation, open the model project\n');
-fprintf('and execute the following command:\n');
+fprintf('Finished creating component %s at\n', componentName);
+fprintf('  %s\n', compDir);
+fprintf('To view model and run simulation,\n');
+fprintf('change to the \\test subfolder and run:\n');
 fprintf('    >> %s\n', testScript);
 
-proj.close();
 
 end
 
